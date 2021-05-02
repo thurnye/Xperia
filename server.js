d938b4196b10a0b21d0bbaadd55f968b5cc36ac2
Daniel Tamunotonye <dan_tonye@Gudi.local>
2021-05-02 10:04:19 -0400

    deploy

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(express.json());
 
 
-
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 
